c1cb2a35e2dac18e99408948f3af2e83d4a82a09
Karthik Kumar <Karthikkumar7022@gmail.com>
2021-07-24 11:46:03 +0530

    solutions

--- hackerRankAssignments/src/18.count_substring.py
+++ hackerRankAssignments/src/18.count_substring.py
@@ -1,13 +1,22 @@
-def count_substring(string, sub_string):
+def count_substring(first_string, second_string):
+    length_of_first_string = len(first_string)
+    length_of_second_string = len(second_string)
     counter = 0
-    # for sub_string in string:
-        # if sub_string
-        # counter += 1
+    for i in range(1, length_of_first_string - length_of_second_string + 2): # 2 is added to reduced number of runs
+        print(
+            f"{list(first_string)[i - 1:length_of_second_string + i - 1]} and {list(second_string)}")
+        print(True if (list(first_string)[
+                       i - 1:length_of_second_string + i - 1] == list(
+            second_string)) else False)
+        if list(first_string)[i - 1:length_of_second_string + i - 1] == list(
+            second_string):
+            counter += 1
     return counter
 
 
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
-    count = count_substring(string, sub_string)
-    print(count)
+    print(f"{string} and  {sub_string}")
+    counter = count_substring(string, sub_string)
+    print(counter)
